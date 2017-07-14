9ed4eb19df8adc02053c7e4595f5888cbaba4813
HeQuanLi <762966575@qq.com>
2017-07-14 16:00:58 +0800

    加载数据

---
+++ module_home/src/main/java/com/hunter/moduledemo/adapter/HomeListAdapter.java
@@ -0,0 +1,27 @@
+package com.hunter.moduledemo.adapter;
+
+import android.widget.ImageView;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.example.modulebaselib.utils.imageload.ImageLoaderUtils;
+import com.hunter.moduledemo.R;
+import com.hunter.moduledemo.mvp.bean.MeiZhiBean;
+
+/**
+ * Created by HeQuanli on 2017/7/14.
+ * 类说明：主页列表数据适配器
+ */
+
+public class HomeListAdapter extends BaseQuickAdapter<MeiZhiBean, BaseViewHolder> {
+
+    public HomeListAdapter() {
+        super(R.layout.item_list_home);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder holder, MeiZhiBean item) {
+        ImageLoaderUtils.dispalyImage(item.getUrl(), (ImageView) holder.getView(R.id.iv_pic));
+        holder.setText(R.id.tv_date, item.getPublishedAt());
+    }
+}
--- module_home/src/main/java/com/hunter/moduledemo/mvp/view/HomeActivity.java
+++ module_home/src/main/java/com/hunter/moduledemo/mvp/view/HomeActivity.java
@@ -1,10 +1,13 @@
 package com.hunter.moduledemo.mvp.view;
 
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.example.modulebaselib.base.BaseActivity;
 import com.example.modulebaselib.base.BaseApplication;
 import com.hunter.moduledemo.R;
+import com.hunter.moduledemo.adapter.HomeListAdapter;
 import com.hunter.moduledemo.dagger2.component.DaggerHomeActivityComponent;
 import com.hunter.moduledemo.dagger2.module.HomeActivityModule;
 import com.hunter.moduledemo.mvp.bean.MeiZhiBean;
@@ -15,20 +18,13 @@ import java.util.List;
 
 import butterknife.BindView;
 
-public class HomeActivity extends BaseActivity<HomeListPresenter> implements HomeListContract.View {
+public class HomeActivity extends BaseActivity<HomeListPresenter>
+        implements HomeListContract.View, BaseQuickAdapter.RequestLoadMoreListener {
 
     @BindView(R.id.rv_main_list)
     RecyclerView mRvMainList;
 
-    @Override
-    public void showErrorMsg(String errorMsg) {
-
-    }
-
-    @Override
-    public void displayData(List<MeiZhiBean> list) {
-
-    }
+    private HomeListAdapter mAdapter;
 
     @Override
     protected void initInject() {
@@ -46,5 +42,28 @@ public class HomeActivity extends BaseActivity<HomeListPresenter> implements Hom
     @Override
     protected void initViewAndEvent() {
         mPresenter.getMeiZhiData("福利", this);
+
+        mAdapter = new HomeListAdapter();
+        mAdapter.setEnableLoadMore(true);
+        mAdapter.setOnLoadMoreListener(this, mRvMainList);
+
+        GridLayoutManager layout = new GridLayoutManager(this, 2);
+        mRvMainList.setLayoutManager(layout);
+        mRvMainList.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void showErrorMsg(String errorMsg) {
+
+    }
+
+    @Override
+    public void displayData(List<MeiZhiBean> list) {
+        mAdapter.addData(list);
+    }
+
+    @Override
+    public void onLoadMoreRequested() {
+
     }
 }
